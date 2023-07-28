c6794edf5d8799da019bd1b6bbe31284ba67ba1b
chauhan2006 <chauhan2006@gmail.com>
2023-07-28 21:50:03 +0530

    add some logic for credit usage

--- src/main/java/com/example/creditcardoptimizer/mocks/CreditAccountAgeMock.java
+++ src/main/java/com/example/creditcardoptimizer/mocks/CreditAccountAgeMock.java
@@ -12,7 +12,7 @@ public class CreditAccountAgeMock implements Mock<CreditAccount> {
     @Override
     public CreditAccount generate() {
         Faker faker = new Faker();
-        LocalDate date = LocalDate.of(faker.random().nextInt(2000,2020),faker.random().nextInt(1,12),faker.random().nextInt(1,31 ));
+        LocalDate date = LocalDate.of(faker.random().nextInt(2000,2020),faker.random().nextInt(1,12),faker.random().nextInt(1,28 ));
         return new CreditAccount(date);
     }
 }
