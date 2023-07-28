a422145664f4bdd0b654543bb293fe21c988342e
Sparsh <sparshaggarwal1993@yahoo.com.au>
2023-07-28 14:12:51 +0530

    added class

--- src/main/java/mocks/PaymentHistoryMock.java
+++ src/main/java/mocks/PaymentHistoryMock.java
@@ -1,6 +1,23 @@
 package mocks;
 
-public class PaymentHistoryMock {
+import com.github.javafaker.Faker;
+import models.Payment;
+
+import java.util.concurrent.TimeUnit;
+
+public class PaymentHistoryMock implements Mock{
 
 
+    @Override
+    public Object generate() {
+        Faker faker = new Faker();
+        Payment payment = new Payment();
+
+        payment.setPaymentId(faker.number().digits(8));
+        payment.setPaymentDate(faker.date().past(30, TimeUnit.DAYS).toString()); // Get a date from the last 30 days
+        payment.setAmount(faker.number().randomDouble(2, 10, 1000));
+        payment.setPaymentMethod(faker.options().option("Credit Card", "PayPal", "Bank Transfer"));
+
+        return payment;
+    }
 }
